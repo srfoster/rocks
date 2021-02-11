048eb3face28f283265847ee5d010a0918abe381
Lindsey Handley <lindseyhandley@gmail.com>
2021-02-11 19:08:41 +0000

    updated readme, gitignore, pushing unreal

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 rocks
 =====
 `rocks` is a CodeSpells mod! Learn more at https://codespells.org.
+
+### Installation
+
+Clone this repo
+
+```
+cd rocks
+raco pkg install
+raco codespells-install
+```
+
+Open up `main.rkt` and adjust the `codespells-workspace` accordingly. Run.
+
+### Including this Rune Collection as a Package
+
+`(require-mod rocks)`
+
+This gives you access to `rocks:my-mod-lang` which you can combine into your mod's rune lang with `append-rune-langs`.
+
+### Unreal Development
+
+If you want to start up the Unreal project (`/Unreal`), install the following:
+* Huge Rocks Pack: https://www.unrealengine.com/marketplace/en-US/product/huge-rocks-pack
+  * Place in: `Plugins/Rocks/Content/HugeRocksPack`
+
+### Releasing New Content
+
+If you have only changed the Racket code, just push to Github.
+
+If you have changed or added Unreal assets, push to Github and run `raco codespells-release`.
--- main.rkt
+++ main.rkt
@@ -9,36 +9,42 @@
 
 
 (define-classic-rune (gnarly-rock [version 1]) ;Note for docs: Only version = 1 works, but there are many rocks in the Mod that can be exposed as BPs
-  #:background "brown"
+  #:background "red"
   #:foreground (bitmap/url "https://static.thenounproject.com/png/1747907-200.png")
-  (spawn-mod-blueprint pak-folder mod-name (~a "GnarlyRock" version)))
+  (with-scale 6
+    (spawn-mod-blueprint pak-folder mod-name (~a "GnarlyRock" version))))
 
 (define-classic-rune (forest-rock [version 1]) ;Note for docs: Only version = 1 works, but there are many rocks in the Mod that can be exposed as BPs
-  #:background "brown"
+  #:background "red"
   #:foreground (bitmap/url "https://static.thenounproject.com/png/2614278-200.png")
-  (spawn-mod-blueprint pak-folder mod-name (~a "ForestRock" version)))
+  (with-scale 6
+    (spawn-mod-blueprint pak-folder mod-name (~a "ForestRock" version))))
 
 (define-classic-rune (black-rock [version 1]) ;Note for docs: Only version = 1 works, but there are many rocks in the Mod that can be exposed as BPs
-  #:background "brown"
+  #:background "red"
   #:foreground (bitmap/url "https://static.thenounproject.com/png/438784-200.png")
-  (spawn-mod-blueprint pak-folder mod-name (~a "BlackRock" version)))
+  (with-scale 6
+    (spawn-mod-blueprint pak-folder mod-name (~a "BlackRock" version))))
 
 (define-classic-rune (coal [version 1]) ;Note for docs: Only version = 1 works, but there are many rocks in the Mod that can be exposed as BPs
-  #:background "brown"
+  #:background "red"
   #:foreground (bitmap/url "https://static.thenounproject.com/png/16776-200.png")
-  (spawn-mod-blueprint pak-folder mod-name (~a "Coal" version)))
+  (with-scale 12
+    (spawn-mod-blueprint pak-folder mod-name (~a "Coal" version))))
 
 
 (define-classic-rune (metallic-rock [version 1]) ;Note for docs: Only version = 1 works, but there are many rocks in the Mod that can be exposed as BPs
-  #:background "brown"
+  #:background "red"
   #:foreground (bitmap/url "https://static.thenounproject.com/png/746834-200.png")
   ;Metalic is mispelled in the Unreal project
-  (spawn-mod-blueprint pak-folder mod-name (~a "MetalicSmallRock" version)))
+  (with-scale 18
+    (spawn-mod-blueprint pak-folder mod-name (~a "MetalicSmallRock" version))))
 
 (define-classic-rune (floor-mossed-stone [version 1]) ;Note for docs: Only version = 1 works, but there are many rocks in the Mod that can be exposed as BPs
-  #:background "brown"
+  #:background "red"
   #:foreground (bitmap/url "https://static.thenounproject.com/png/438790-200.png") 
-  (spawn-mod-blueprint pak-folder mod-name (~a "FloorMossedStone" version)))
+  (with-scale 2
+    (spawn-mod-blueprint pak-folder mod-name (~a "FloorMossedStone" version))))
 
 (define-classic-rune-lang my-mod-lang #:eval-from main.rkt
   (gnarly-rock forest-rock black-rock coal
